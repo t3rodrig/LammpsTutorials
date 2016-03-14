a1475d64bf28f04727b3cd11a3714d7b485f6813
Yuriy Khalak <khalak.yura@gmail.com>
2016-03-14 15:34:17 +0100

    Added an analysis script to calculate properties of the dipole moment of the simulation cell.

---
+++ analysis/dipole.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import numpy as np
+import scipy as sp
+import MDAnalysis
+import matplotlib.pyplot as plt
+
+
+
+###############
+#Entry point:
+###############
+
+
+#load topology and trajectory
+topFname="../liquid-vapor/data.spce.old.txt"    #topology_format="DATA"
+trajFname="../liquid-vapor/traj.dcd"            #format="LAMMPS"
+u = MDAnalysis.Universe(topFname, trajFname, topology_format="DATA", format="LAMMPS")
+all_atoms = u.select_atoms("all")
+
+mu_history=[]
+#itterate through frames
+for ts in u.trajectory:
+    #calculate the total dipole moment of the simulation cell
+    temp=np.multiply(all_atoms.charges[:,np.newaxis], all_atoms.coordinates()) #Q*r
+    mu=np.sum(temp, axis=0) #sum(Q*r)
+    muMag=np.linalg.norm(mu)/0.20819434 #magnitude in Debye
+    mu_history.append(muMag)
+
+#build histogram
+mu_history=np.array(mu_history)
+max_mu=np.amax(mu_history)
+min_mu=np.amin(mu_history)
+avg=np.mean(mu_history)
+print "<mu>=",avg,"Debye"
+print "<mu^2>=",np.mean(mu_history**2),"Debye"
+
+
+step = 10    #spacing between points on the histogram in Debye
+#shift max and min so that ends of the histogram are zero
+min_mu-=2*step
+max_mu+=2*step
+
+histogram=np.zeros(int((max_mu-min_mu)/step)+1) #allocate space
+x=np.linspace(min_mu, min_mu+step*histogram.shape[0], histogram.shape[0])
+for mu in mu_history:
+    i=int((mu-min_mu)/step)
+    histogram[i]+=1         #increment count for entries in this region
+
+#TODO: normalize the distribution
+
+
+#draw and save plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlabel('Total Dipole Moment (Debye)')
+ax.set_ylabel('Number of Occurances')
+ax.plot(x, histogram, '-')
+
+fig.savefig("dipole.png")
+
+exit()
